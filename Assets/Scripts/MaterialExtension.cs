727558692398286d2ba8d14a6170424fd144c7ee
SitronX <68167377+SitronX@users.noreply.github.com>
2023-04-13 14:36:59 +0200

    Fix
    +Fixed incorrect alpha material behaviour on builds (shader stripping issue)

--- Assets/Scripts/MaterialExtension.cs
+++ Assets/Scripts/MaterialExtension.cs
@@ -7,7 +7,8 @@ public static class MaterialExtensions
     //https://forum.unity.com/threads/change-rendering-mode-via-script.476437/#post-5235491
     public static void ToOpaqueMode(this Material material)
     {
-        material.SetOverrideTag("RenderType", "");
+        material.SetFloat("_Mode", 0f);
+
         material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
         material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
         material.SetInt("_ZWrite", 1);
@@ -19,13 +20,15 @@ public static class MaterialExtensions
 
     public static void ToFadeMode(this Material material)
     {
-        material.SetOverrideTag("RenderType", "Transparent");
+        material.SetFloat("_Mode", 2f);
+
         material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
         material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
         material.SetInt("_ZWrite", 0);
         material.DisableKeyword("_ALPHATEST_ON");
         material.EnableKeyword("_ALPHABLEND_ON");
         material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-        material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+        material.renderQueue = 3000;
+
     }
 }
